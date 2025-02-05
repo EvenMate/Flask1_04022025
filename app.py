dd903885d099aadacdcda740219a73678fb37f64
Evgeny Kurskikh <epersic@mail.ru>
2025-02-05 21:35:08 +0300

    POST requets done

--- app.py
+++ app.py
@@ -1,25 +1,31 @@
 from flask import Flask
 import random as rd
 
+from flask import request
+
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
-all_quotes = ["цитата с id 0", "цитата с id 1", "цитата с id 2", "цитата с id 3", "цитата с id 4", "цитата с id 5"]
+all_quotes = [{"id": 0,"quote": "нулевая цитата"}, {"id": 1,"quote": "первая цитата"}, {"id": 2,"quote": "вторая цитата"}, {"id": 3,"quote": "третья цитата"}]
+
 
 about_me = {
     "name": "Евгений",
     "surname": "Курских",
     "email": "epersic@mail.ru"
-
 }
+
 @app.route("/")
 def hello_world():
     return "Hello, World!"
 
-
 @app.route("/about")
 def about():
     return about_me
+
+@app.route("/quotes") 
+def get_all_quotes():
+    return all_quotes
 
 @app.route("/quotes/<int:id>") ## Задание 1 и 2 из Практика - часть 1
 def get_quotes(id):
@@ -32,9 +38,16 @@ def get_quotes(id):
 def get_quotes_count():
     return {"count": str(len(all_quotes))}
 
-@app.route("/quotes/rnd") ## Задание 4 из Практика - часть 1
+@app.route("/quotes/random") ## Задание 4 из Практика - часть 1
 def get_random_quote():
     return all_quotes[rd.randint(0, len(all_quotes)-1)]
+
+@app.route("/quotes", methods=['POST']) # для Практики части 2 
+def create_quote():
+   data = request.json
+   print("data = ", data)
+   all_quotes.append({"id": len(all_quotes), "quote":data["text"]}) 
+   return data, 201
 
 if __name__ == "__main__":
     app.run(debug=True)
